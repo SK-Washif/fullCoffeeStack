3cde482a15c81fb327fdf6b51f71df435f503da3
SK-Washif <washiffishal@gmail.com>
2025-11-08 00:39:59 +0600

    Delete user properly and signin user page ready

--- coffee-store-client/src/components/Signin.jsx
+++ coffee-store-client/src/components/Signin.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 
 const Signin = () => {
+
+  const handleSignIn = e =>{
+    e.preventDefault();
+  }
+
   return (
-    <div>
-      
-    </div>
+    <div className="card bg-base-100 max-w-sm mx-auto shrink-0 shadow-2xl">
+            <div className="card-body">
+              <h1 className="text-5xl font-bold">Sign In now!</h1>
+              <form onSubmit={handleSignIn} className="fieldset">
+                
+                <label className="label">Email</label>
+                <input type="email" name='email' className="input" placeholder="email" />
+                <label className="label">Password</label>
+                <input type="password" name='password' className="input" placeholder="Password" />
+                <div><a className="link link-hover">Forgot password?</a></div>
+                <button className="btn btn-neutral mt-4">Sign In</button>
+              </form>
+            </div>
+          </div>
   )
 }
 
